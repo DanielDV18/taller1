abca96de3fb2273df5d01f36ce3bbbf7139e9094
Daniel <ddaza681@gmail.com>
2022-08-09 17:07:11 -0500

    Se hizo el juego cara y sello

--- js/cari.js
+++ js/cari.js
@@ -1,11 +1,28 @@
+let i=true;
+let contador=0;
+let acomulador=0;
 
-let random = Math.floor(Math.random(1)*(2));
-let i = true;
+
 while(i=true){
-    elecction=prompt("Elija un numero entre 1 para sello y 2 para cara");
-    din=prompt("¿cuanto desea apostar?");
-    window.alert(`Usted aposto ${din}`);
+    let elec=parseInt(prompt("ingrese 1=sello o 2=cara"));
+    let apuesta=prompt(`Ingrese la cantidad de su apuesta para el cara o sello`);
+    let random=Math.floor(Math.random(1)*2); 
+    acomulador=parseInt(acomulador)+apuesta;
+    window.alert(`Usted aposto ${apuesta}`);
+    window.alert(`El resultado fue ${random}`);
 
-
+if(elec=1,0){
+    window.alert(`Gano el juego :D con el numero ${random}`);
+    window.alert(`Gano la apuesta con ${acomulador} dinero`);
+}else if(elec=2){
+    
+    window.alert("Ash, perdiste el juego D:");
+    window.alert(`Perdiste con ${acomulador} dinero`);
+    
 
 }
+contador=contador+1;
+window.alert (`Las veces que usted jugo fueron ${contador}`)
+i=confirm("¿Desea continuar?");
+}
+
