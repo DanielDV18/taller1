9ba863087526e091eaedfe9c96cd4044dd22978f
Daniel <ddaza681@gmail.com>
2022-08-23 17:55:53 -0500

    Solo falta el 3, se hizo el primero y se le dio estilo

--- js/ejer.js
+++ js/ejer.js
@@ -1,13 +1,16 @@
-let nom=prompt("Digite su nombre");
-//NOMBRE EN MAYUSCULAS
-window.alert("Su nombre en mayusculas es: "+nom.toUpperCase());
-//CARACTERES EN MAYUSCULAS
-window.alert("Su longitud de caracteres es: "+nom.length);
+function calcular(){
+    let pa=document.getElementById("pal").value;
+    let op=document.getElementById("opciones").value;
 
-let ap=prompt("Digite su apellido");
-//APELLIDO EN MAYUSCULAS
-window.alert("Su apellido en mayusculas es: "+ap.toUpperCase());
-//CARACTERES DE APELLIDO
-window.alert("Su longitud de caracteres: "+ap.length);
-//Uniendo nombre y apellido
-window.alert("Su usario seria: ");
+    if (op==1) {
+        window.alert("Su longitud de caracteres es: "+pa.length);
+    }else if (op==2) {
+        window.alert("Su nombre en mayusculas es: "+pa.toUpperCase());
+    }else if (op==3) {
+        window.alert("Su nombre en minisculas es: "+pa.toLowerCase());
+    }else if (op==4) {
+        window.alert("La primer letra es: "+pa.charAt(0));
+    }else{
+        alert("Error 404")
+    }
+}
