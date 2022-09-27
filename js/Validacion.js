5ccee61d81094f6c9a4e4fbe1afe53230da32032
Daniel <ddaza681@gmail.com>
2022-09-26 22:19:10 -0500

    Se termino los modales y se realizo en un 90% validaciones

--- js/Validacion.js
+++ js/Validacion.js
@@ -6,104 +6,160 @@
 
 // Este es para el password ^(?=.+\d) (?=.*[#$%@&!¡]) (?=#.*[a-z]) (?=#.*[A-Z]).{8,}$
 
-const reglas={
-    textos:/^[a-zA-ZÁ-ÿ\s]{1,20}$/,
-    numeros:/^\d{8,10}$/,
-    correos:/^[a-zA-Z0-9+-.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    password:/^(?=.+\d)(?=.*[#$%&!@])(?=.*[a-z])(?=.*[A-Z]).{5,}$/
+const reglas = {
+    textos: /^[a-zA-ZÁ-ÿ\s]{1,20}$/,
+    numeros: /^\d{8,10}$/,
+    correos: /^[a-zA-Z0-9+-.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    password: /^(?=.+\d)(?=.*[#$%&!@])(?=.*[a-z])(?=.*[A-Z]).{5,}$/
 }
-
+const inputs = {
+    numdoc: false,
+    nombre: false,
+    apellido: false,
+    correo: false,
+    telefono: false,
+    password: false
+}
 //Aceder al formulario
-let form=document.getElementById("frm-usuario");
-let campos=document.querySelectorAll("#frm-usuario input");
+let form = document.getElementById("frm-usuario");
+let campos = document.querySelectorAll("#frm-usuario input");
 
 
 //Agregar el listener al evento submit
-form.addEventListener('submit',e=>{
+form.addEventListener('submit', e => {
     e.preventDefault(); //Evita que se envie el formulario para enviar validaciones
 })
 
-const validarInput=(regla,input,grupo)=>{
-    if(regla.test(input.value)){
+const validarInput = (regla, input, grupo) => {
+    if (regla.test(input.value)) {
         document.getElementById(`g-${grupo}`).classList.remove('error');
-            document.getElementById(`g-${grupo}`).classList.add('succes');
-            document.querySelector(`#g-${grupo} i`).classList.add('fa-circle-check');
-            document.querySelector(`#g-${grupo} i`).classList.remove('fa-circle-exclamation');
-            document.querySelector(`#g-${grupo} .msn-error`).classList.remove('msn-error-visible')
+        document.getElementById(`g-${grupo}`).classList.add('succes');
+        document.querySelector(`#g-${grupo} i`).classList.add('fa-circle-check');
+        document.querySelector(`#g-${grupo} i`).classList.remove('fa-circle-exclamation');
+        document.querySelector(`#g-${grupo} .msn-error`).classList.remove('msn-error-visible')
 
-        }else{
-            document.getElementById(`g-${grupo}`).classList.add('error');
-            document.getElementById(`g-${grupo}`).classList.remove('succes');
-            document.querySelector(`#g-${grupo} i`).classList.remove('fa-circle-check');
-            document.querySelector(`#g-${grupo} i`).classList.add('fa-circle-exclamation');
-            document.querySelector(`#g-${grupo} .msn-error`).classList.add('msn-error-visible')
-
+    } else {
+        document.getElementById(`g-${grupo}`).classList.add('error');
+        document.getElementById(`g-${grupo}`).classList.remove('succes');
+        document.querySelector(`#g-${grupo} i`).classList.remove('fa-circle-check');
+        document.querySelector(`#g-${grupo} i`).classList.add('fa-circle-exclamation');
+        document.querySelector(`#g-${grupo} .msn-error`).classList.add('msn-error-visible')
+        inputs[grupo] = true;
     }
 }
-const validarCampos=(e)=>{
-    console.log("Se genero un evento en el campo "+e.target.name);
-    switch(e.target.name){
+const validarCampos = (e) => {
+    console.log("Se genero un evento en el campo " + e.target.name);
+    switch (e.target.name) {
         case "numdoc":
-            validarInput(reglas.numeros,e.target ,e.target.name);
-        break;
+            validarInput(reglas.numeros, e.target, e.target.name);
+            break;
 
 
         case "nombre":
-            validarInput(reglas.textos,e.target ,e.target.name);
-        break;
+            validarInput(reglas.textos, e.target, e.target.name);
+            break;
 
 
 
         case "apellido":
-            validarInput(reglas.textos,e.target ,e.target.name);
-        break;
+            validarInput(reglas.textos, e.target, e.target.name);
+            break;
 
 
 
         case "correo":
-            validarInput(reglas.correo,e.target ,e.target.name);
-        break;
+            validarInput(reglas.correos, e.target, e.target.name);
+            break;
+
+
+
 
         case "telefono":
-            validarInput(reglas.numeros,e.target ,e.target.name);
-        break;
+            validarInput(reglas.numeros, e.target, e.target.name);
+            break;
+
+
+
 
         case "password":
-            validarInput(reglas.password,e.target ,e.target.name);
-        break;
+            validarInput(reglas.password, e.target, e.target.name);
+            break;
+
+        case "password2":
+            validarPassword();
+            break;
+
+
+
         default:
             alert("no se recibio un evento sobre el input");
-        break;
+            break;
+    }
 }
+/**Validar Password*/
+const validarPassword = () => {
+    const pass1 = document.getElementById('password');
+    const pass2 = document.getElementById('password2');
+
+    if (pass1.value === pass2.value) {
+        
+        document.getElementById(`g-password2`).classList.add('success');
+        
+        document.querySelector("#g-password2 i").classList.add('fa-circle-check')
+        
+        document.getElementById(`g-password2`).classList.remove('error');
+        
+        document.querySelector("#g-password2 i").classList.remove('fa-circle-exclamation')
+
+        document.querySelector(`#g-password2 .msn-error`).classList.remove('msn-error-visible');
+
+        inputs['password'] = true;
+    } else {
+        document.getElementById('g-password2').classList.add('error');
+
+        document.querySelector("#g-password2 .msn-error").classList.add('msn-error-visible');
+
+        document.querySelector("#g-password2 i").classList.remove('fa-circle-check')
+
+        document.querySelector("#g-password2 i").classList.add('fa-circle-exclamation')
+        inputs['password'] = false;
+    }
 }
 
 
-campos.forEach((campo)=>{
-    campo.addEventListener("keyup",validarCampos);
-    campo.addEventListener("blur",validarCampos);
+campos.forEach((campo) => {
+    campo.addEventListener("keyup", validarCampos);
+    campo.addEventListener("blur", validarCampos);
 
 })
 
-/*const validarCampos=(e)=>{
-    console.log("Se genero un evento en el campo "+e.target.name);
-    switch(e.target.name){
-        case "numdoc":
-        if(reglas.numeros.test(e.target.value)){
-            document.getElementById('g-numdoc').classList.remove('error');
-            document.getElementById('g-numdoc').classList.add('succes');
-            document.querySelector('#g-numdoc i').classList.add('fa-circle-check');
-            document.querySelector('#g-numdoc i').classList.remove('fa-circle-exclamation');
-            document.querySelector("#g-numdoc .msn-error").classList.remove('msn-error-visible')
-
-        }else{
-            document.getElementById('g-numdoc').classList.add('error');
-            document.getElementById('g-numdoc').classList.remove('succes');
-            document.querySelector('#g-numdoc i').classList.remove('fa-circle-check');
-            document.querySelector('#g-numdoc i').classList.add('fa-circle-exclamation');
-            document.querySelector("#g-numdoc .msn-error").classList.add('msn-error-visible')
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const terminos = document.getElementById("terminos");
 
 
-        }
-        
-        break;
-*/
+
+    if (inputs.numdoc && inputs.nombre && inputs.apellido && inputs.correo
+        && inputs.telefono && inputs.password && terminos.checked) {
+
+        alert("El usuario a sido validado");
+        form.reset()
+        document.querySelectorAll('.success').forEach(icono => {
+            icono.classList.remove('success')
+        })
+
+    }
+    else if (!inputs==false) {
+        document.querySelectorAll('.success').forEach(icono => {
+            icono.classList.remove('error')
+        })
+        document.querySelectorAll('.msn-error').forEach(icono => {
+            icono.classList.add('msn-error-visible')
+            console.log("Se genero un error")
+        })
+
+    }
+    else{
+        console.log("sigio derecho");
+    }
+})
